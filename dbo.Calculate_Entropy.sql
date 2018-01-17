1f7d274008f23c20199671880f92dd50ea15984a
sfibich <steve.fibich@gmail.com>
2018-01-17 13:07:07 -0500

    Cannot "Compile"
    Added data set to proc object file since they are related....not great but works better for working with object currently

--- dbo.Calculate_Entropy.sql
+++ dbo.Calculate_Entropy.sql
@@ -1,7 +1,34 @@
+if exists (select 1 from sys.procedures where name = 'Calculate_Entropy')
+	drop procedure dbo.Calculate_Entropy;
 
+if exists (select 1 from sys.types where name = 'Entropy_Dataset')
+	drop type dbo.Entropy_Dataset;
+go
+CREATE type dbo.Entropy_Dataset AS TABLE(
+Member_Name nvarchar(500) not null
+,Member_Count decimal(9,0) not null
+);
+
+go
 
 CREATE OR ALTER PROCEDURE dbo.Calculate_Entropy 
+@Entropy_Dataset Entropy_Dataset READONLY
+,@Entropy decimal(9,8) OUTPUT
 AS
 BEGIN
-	print 'STUB!';
+
+	select
+	ed.Member_Name
+	,ed.Member_Count
+	,sum(ed.Member_Count) over() as total_member_count
+	,ed.Member_Count/sum(ed.Member_Count) over()
+	,log(ed.Member_Count/sum(ed.Member_Count) over()) as [log]
+--	,sum(ed.Member_Count) over()/ed.Member_Count * log(sum(ed.Member_Count) over()/ed.Member_Count) as part_entropy
+	from
+	@Entropy_Dataset ed
+
 END
+
+go
+
+
--- tests/calculate_entropy_tests.sql
+++ tests/calculate_entropy_tests.sql
@@ -36,7 +36,7 @@ BEGIN
 	from
 	(values
 		('Member_Name','nvarchar',0,0,0,1000)
-		,('Member_Count','bigint',0,19,0,8)
+		,('Member_Count','decimal',0,9,0,5)
 	) as Expected(column_name,data_type_name,is_nullable,precision,scale,max_length)
 
 	/*Execute*/
@@ -69,5 +69,133 @@ BEGIN
 
 END
 go
+
+
+
+CREATE OR ALTER PROCEDURE Test_Calculate_Entropy.Test__Interface__Input__dbo_Calculate_Entropy 
+as
+BEGIN
+
+	/*Set Up*/
+	select
+	column_name
+	,data_type_name
+	,is_nullable
+	,precision
+	,scale,max_length
+	into #Expected
+	from
+	(values
+		('@Entropy_Dataset','Entropy_Dataset',1,0,0,-1)
+	) as Expected(column_name,data_type_name,is_nullable,precision,scale,max_length)
+
+	/*Execute*/
+	select
+	par.name as[column_name]
+	,t.name as [data_type_name]
+	,par.is_nullable
+	,par.precision
+	,par.scale
+	,par.max_length
+	into #Actual
+	from
+	sys.procedures p
+	inner join
+	sys.parameters par
+	on
+	par.object_id=p.object_id
+	inner join
+	sys.types t
+	on
+	par.user_type_id=t.user_type_id
+	where
+	p.name='Calculate_Entropy'
+	and
+	p.schema_id=schema_id('dbo')
+	and
+	par.is_output =0
+
+
+	/*Test*/
+	execute tSQLt.AssertEqualsTable @Expected='#Expected',@Actual='#Actual'
+END
+go
+
+
+
+CREATE OR ALTER PROCEDURE Test_Calculate_Entropy.Test__Interface__Output__dbo_Calculate_Entropy 
+as
+BEGIN
+
+	/*Set Up*/
+	select
+	column_name
+	,data_type_name
+	,is_nullable
+	,precision
+	,scale,max_length
+	into #Expected
+	from
+	(values
+		('@Entropy','decimal',1,9,8,5)
+	) as Expected(column_name,data_type_name,is_nullable,precision,scale,max_length)
+
+	/*Execute*/
+	select
+	par.name as[column_name]
+	,t.name as [data_type_name]
+	,par.is_nullable
+	,par.precision
+	,par.scale
+	,par.max_length
+	into #Actual
+	from
+	sys.procedures p
+	inner join
+	sys.parameters par
+	on
+	par.object_id=p.object_id
+	inner join
+	sys.types t
+	on
+	par.user_type_id=t.user_type_id
+	where
+	p.name='Calculate_Entropy'
+	and
+	p.schema_id=schema_id('dbo')
+	and
+	par.is_output =1
+
+	/*Test*/
+	execute tSQLt.AssertEqualsTable @Expected='#Expected',@Actual='#Actual'
+END
+go
+
+
+CREATE OR ALTER PROCEDURE Test_Calculate_Entropy.Test__dbo_Calculate_Entropy__2__Row__Input 
+as
+BEGIN
+
+	/*Set Up*/
+	declare @Entropy_Dataset Entropy_Dataset;
+	declare @Actual_Entropy decimal(9,8);
+	declare @Expected_Entropy decimal(9,8)=.88;
+
+	insert into @Entropy_Dataset
+	Values('Group 1',7);
+	insert into @Entropy_Dataset
+	Values('Group 2',3);
+
+
+	/*Execute*/
+	execute dbo.Calculate_Entropy
+	@Entropy_Dataset = @Entropy_Dataset 
+	,@Entropy=@Actual_Entropy OUTPUT;
+
+	/*Test*/
+	execute tSQLt.AssertEquals @Expected=@Expected_Entropy,@Actual=@Actual_Entropy
+END
+go
+
 
 exec tSQLt.RunAll;
